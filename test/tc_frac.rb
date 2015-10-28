c32b62aa41013df29baa7710fcd08664e1f19a90
Oscar Catari Gutierrez <alu0100825893@ull.edu.es>
2015-10-28 06:53:21 +0000

    Comprobacion de tipos y denominador distinto de cero

--- test/tc_frac.rb
+++ test/tc_frac.rb
@@ -9,5 +9,10 @@ class TestFraccionario < Test::Unit::TestCase
     def test_simple
         assert_equal("(0,1)", @cero.to_s)
         assert_equal("(1,1)", @unidad.to_s)
+        assert_raise(RuntimeError) {Fraccionario.new(1,0)}
     end
+    def test_type_check
+        assert_raise(RuntimeError) {Fraccionario.new('1','1')}
+    end
+    
 end
